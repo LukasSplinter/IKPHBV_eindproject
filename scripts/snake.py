abae04ead3daaa73ff187657cc83a16436b47e33
lukas splinter <lukas.splinter@gmail.com>
2021-01-31 22:51:46 +0100

    final changes snake.py

--- scripts/snake.py
+++ scripts/snake.py
@@ -108,6 +108,17 @@ class App:
     player = 0
     syringe = 0
 
+    isAlive = True
+
+    #text which is displayed on ending of game
+    endText = "Net zoals dit spel, is de pandemie beter te overwinnen met een mondkapje."
+    endSubtext = "Draag hierom altijd een mondkapje als je naar buiten gaat!"
+    #color for end game text
+    WHITE = (255,255,255)
+
+    #time for which endscreen is displayed
+    endScreenTimer = 10
+
     # ranges 0 <-> 29 || 0 ↨ 19
     # max range = windowWidth(or height)/44(tilesize)-1(index)
     spawnLimitSyringes = {"X1": 0, "X2": 29,
@@ -252,27 +263,40 @@ class App:
         # does snake get corona
         for i in range(0, self.player.length):
             for virus in App.viruses:
-                if self.game.isCollision(virus.x, virus.y, self.player.x[i], self.player.y[i], 44):
-                    exit(0)
-
+                if self.game.isCollision(virus.x, virus.y, self.player.x[i], self.player.y[i], 40):
+                    App.on_end(self)
         # does snake collide with itself?
         for i in range(2, self.player.length):
             if self.game.isCollision(self.player.x[0], self.player.y[0], self.player.x[i], self.player.y[i], 40):
-                print("You lose! Collision: ")
-                print("x[0] (" + str(self.player.x[0]) + "," + str(self.player.y[0]) + ")")
-                print("x[" + str(i) + "] (" + str(self.player.x[i]) + "," + str(self.player.y[i]) + ")")
-                exit(0)
+                App.on_end(self)
 
         for i in range(2, self.player.length):
             if self.player.x[0] >= self.windowWidth or self.player.x[0] <= -1:
-                print("Stinkie Border")
-                exit(0)
+                App.on_end(self)
 
             if self.player.y[0] >= self.windowHeight or self.player.y[0] <= -1:
-                print("Stinkie border")
-                exit(0)
-
+                App.on_end(self)
         pass
+
+    def on_end(self):
+        App.syringes.clear()
+        App.viruses.clear()
+        App.isAlive = False
+
+        self.draw_text(self.endText , 36, self.WHITE, self.windowWidth / 2, self.windowHeight / 3.5)
+        self.draw_text(self.endSubtext, 24, self.WHITE, self.windowWidth/2, self.windowWidth / 3)
+        pygame.display.update()
+
+        time.sleep(self.endScreenTimer)
+        exit(0)
+
+    def draw_text(self, text, size, color, x, y):
+        font = pygame.font.SysFont('arial', size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self._display_surf.blit(text_surface, text_rect)
+
 
     def on_render(self):
         self._display_surf.fill((0, 0, 0))
@@ -293,21 +317,22 @@ class App:
         if self.on_init() == False:
             self._running = False
 
-        while (self._running):
+        while self._running:
             pygame.event.pump()
             keys = pygame.key.get_pressed()
 
-            if (keys[K_RIGHT]):
-                self.player.moveRight()
+            if App.isAlive:
+                if (keys[K_RIGHT]):
+                    self.player.moveRight()
 
-            if (keys[K_LEFT]):
-                self.player.moveLeft()
+                if (keys[K_LEFT]):
+                    self.player.moveLeft()
 
-            if (keys[K_UP]):
-                self.player.moveUp()
+                if (keys[K_UP]):
+                    self.player.moveUp()
 
-            if (keys[K_DOWN]):
-                self.player.moveDown()
+                if (keys[K_DOWN]):
+                    self.player.moveDown()
 
             if (keys[K_ESCAPE]):
                 self._running = False
